dc00a9a07d07e6e0df2f6c76a6a8aee763bee445
Andreas Hultgren <me@andreashultgren.se>
2024-08-08 17:03:17 +0200

    Fix test

--- src/DisplayEvent.test.tsx
+++ src/DisplayEvent.test.tsx
@@ -3,7 +3,17 @@ import { DisplayEvent } from "./DisplayEvent";
 
 describe("Event", () => {
   test("renders event at the appropriate time", () => {
-    render(<DisplayEvent {...{ startAt: 1, endAt: 3, title: "Event" }} />);
+    render(
+      <DisplayEvent
+        {...{
+          startAt: 1,
+          endAt: 3,
+          title: "Event",
+          startIsClipped: false,
+          endIsClipped: false,
+        }}
+      />
+    );
 
     expect(screen.getByText("Event")).toHaveStyle("grid-row-start: 1;");
     expect(screen.getByText("Event")).toHaveStyle("grid-row-end: 3;");
